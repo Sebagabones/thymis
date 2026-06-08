9e8b917f00394157e77e68bd55019e40b808a1bf
Sebastian Gazey <sebastian.gazey@aquamonix.com.au>
2026-06-08 05:57:26 +0000

    switch to chromuim

--- controller/thymis_controller/modules/kiosk.py
+++ controller/thymis_controller/modules/kiosk.py
@@ -235,7 +235,7 @@ class Kiosk(modules.Module):
                 enable = true;
                 user = "thymiskiosk";
                 program = ''
-                ${{pkgs.firefox}}/bin/firefox -kiosk -private-window  "{kiosk_url}"
+                ${{pkgs.ungoogled-chromium}}/bin/chromium --app='{kiosk_url}'
                 '';
             }};
         """.strip()
