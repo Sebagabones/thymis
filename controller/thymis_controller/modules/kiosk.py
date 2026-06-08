abe20a7bf5e743840fb3c417d75a54687752351a
Sebastian Gazey <sebastian.gazey@aquamonix.com.au>
2026-06-08 07:43:00 +0000

    ask cage to not draw client dectorations

--- controller/thymis_controller/modules/kiosk.py
+++ controller/thymis_controller/modules/kiosk.py
@@ -234,6 +234,7 @@ class Kiosk(modules.Module):
            services.cage = lib.mkOverride {priority} {{
                 enable = true;
                 user = "thymiskiosk";
+                extraArguments = ["-d"];
                 program = ''
                 ${{pkgs.ungoogled-chromium}}/bin/chromium --kiosk --noerrdialogs --disable-infobars --app='{kiosk_url}'
                 '';
