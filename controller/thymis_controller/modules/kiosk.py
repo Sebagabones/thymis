7e6832464bd80c2d9f07a7ebc2462dc3bc5e8385
Sebastian Gazey <sebastian.gazey@aquamonix.com.au>
2026-06-09 08:35:21 +0000

    renamed screen-rotation service

--- controller/thymis_controller/modules/kiosk.py
+++ controller/thymis_controller/modules/kiosk.py
@@ -255,7 +255,7 @@ class Kiosk(modules.Module):
                 ${{pkgs.ungoogled-chromium}}/bin/chromium --kiosk --noerrdialogs --disable-infobars --app='{kiosk_url}'
                 '';
             }};
-          systemd.services.myservice = {{
+          systemd.services.screen-rotation = {{
                enable = true;
                after = [ "graphical.target" ];
                wantedBy = ["cage-tty1.service"];
