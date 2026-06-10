0374a6b6a4739e7926437f7e21c856c97e9cb2e0
Sebastian Gazey <sebastian.gazey@aquamonix.com.au>
2026-06-10 03:33:04 +0000

    Whooops

--- controller/thymis_controller/modules/kiosk.py
+++ controller/thymis_controller/modules/kiosk.py
@@ -258,7 +258,7 @@ class Kiosk(modules.Module):
               }};
               udev.extraRules = ''
                 KERNEL=="event[0-9]", SUBSYSTEM=="input", ATTRS{{phys}}=="input/ts", ENV{{WL_OUTPUT}}="{xrandr_name}"
-                ATTRS{{phys}}=="input/ts", ENV{LIBINPUT_CALIBRATION_MATRIX}="-1 0 1 0 -1 1"
+                ATTRS{{phys}}=="input/ts", ENV{{LIBINPUT_CALIBRATION_MATRIX}}="-1 0 1 0 -1 1"
                 '';
             }};
             systemd.services.screen-rotation = {{
